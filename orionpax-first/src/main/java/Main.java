68a44b9559e62dff097626104fe1538fb6e50808
orionpax-nl <spam@orionpax.nl>
2014-01-14 18:57:38 +0100

    First setup

---
+++ orionpax-first/src/main/java/Main.java
@@ -0,0 +1,16 @@
+package com.mkyong.util;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Rick Jansen");
+    }
+}
